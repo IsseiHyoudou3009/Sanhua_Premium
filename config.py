42a1c236928da417d26d73b9225ba8532454d97b
IsseiHyoudou3009 <IsseiHyoudou3009@gmail.com>
2025-06-20 08:48:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5779295913")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Carlotta:Carlotta_Premium_Bot@cluster0.vzybzst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
